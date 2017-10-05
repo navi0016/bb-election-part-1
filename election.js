66cf728e6674872fb1d7f4e64e003f82eec0eec1
Navi Mann <navdeep.k.mann@gmail.com>
2017-10-05 17:16:52 -0400

    intergrate bikkin bottom api

--- election.js
+++ election.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  var unorderList = document.querySelector('#list')
+
+
+  $.ajax({
+    url:"https://bb-election-api.herokuapp.com/",
+    method:"GET",
+    dataType: "JSON"
+  }).done(function(response) {
+    console.log('You request was successful');
+    console.log(response.candidates);
+    for (var i = 0; i < response.candidates.length; i++) {
+      response.candidates[i]
+
+      var list = document.createElement('li');
+      list.innerText = 'Name: ' + response.candidates[i].name + ' Votes: ' + response.candidates[i].votes
+
+      unorderList.appendChild(list)
+    
+    }
+  })
 
 });
